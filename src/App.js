bd2115fa2d0c70c1f2a106c78297401bd39817b0
Maxfuente <max.fuentes.a@gmail.com>
2023-06-24 22:34:31 -0400

    final

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 function App() {
   return (
     <div className="App">
      <NavBar/>
-     <ItemListContainer greeting={"Buena Compra"}/>
+     <ItemListContainer greeting ={"Buena Compra"}/>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,7 @@
 const ItemListContainer =({greeting})=>{
     return(
         <div>
-            <h2>(greeting)</h2>
+            <h2>{greeting}</h2>
         </div>
     )
     
